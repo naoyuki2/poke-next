fb639c1d3040acaec69f9bfa29a4daddcaf51ea6
naoyuki <kamaboko912@gmail.com>
2024-01-07 12:28:06 +0900

    Header.tsxを作成

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="flex justify-between bg-cyan-200 p-5 font-bold">
+            <div>ポケモン図鑑</div>
+        </div>
+    )
+}
+
+export default Header
